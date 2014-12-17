863edebac5f5f76e030e36350d17204db1fee92f
Harsh Malewar <harshmalewar@Harshs-MacBook-Air.local>
2014-12-17 02:25:57 -0800

    Added Articles Form

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = 'c9a8e227271c80ad19455df66cf0c39c459d9e75fc93549376c0e8abc28802d254d3bae4b92ea520c8338e3a53024be630b53d36417ca23b1820d4eeaf0c6f01'
